6ed4c910e773465d65ddc0ac8ab106dace030180
Gary Xue <Gary.Xue@reece.com.au>
2018-08-20 22:35:36 +1000

    grab git diff ok

---
+++ src/grab-git-line-diff.ts
@@ -0,0 +1,28 @@
+import { exec } from "child_process";
+import { dirname } from "path";
+
+export function grabGitLineDiff(filename: string, lineNo: number) {
+    return new Promise((resolve, reject) => {
+        try {
+            exec(
+                `git log -L${lineNo},+1:${filename}`,
+                {
+                    cwd: dirname(filename)
+                },
+                (err, stdout, stderr) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    if (stderr) {
+                        reject(stderr);
+                        return;
+                    }
+                    resolve(stdout);
+                }
+            );
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
--- src/vscode-git-log-line.ts
+++ src/vscode-git-log-line.ts
@@ -1,29 +1,47 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+import { grabGitLineDiff } from "./grab-git-line-diff";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-git-log-line" is now active!');
+    console.log(
+        'Congratulations, your extension "vscode-git-log-line" is now active!'
+    );
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vscode-git-log-line.showLineLog', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    let disposable = vscode.commands.registerCommand(
+        "vscode-git-log-line.showLineLog",
+        showLineGitHistory
+    );
 
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
+export function deactivate() {}
+
+async function showLineGitHistory() {
+    const activeTextEditor = vscode.window.activeTextEditor;
+    if (activeTextEditor) {
+        const filename = activeTextEditor.document.fileName;
+        const lineNo = activeTextEditor.selection.start.line;
+        console.log(
+            `getting git history for file ${filename} at line ${lineNo}`
+        );
+        try {
+            const content = await grabGitLineDiff(filename, lineNo);
+            console.log(content);
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    }
 }
